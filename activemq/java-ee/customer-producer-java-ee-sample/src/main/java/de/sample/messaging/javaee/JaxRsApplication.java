4bc3f80b1f44655779f71d5455e3534b24769a30
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2022-04-07 05:09:08 +0200

    add first JAX-RS resource to produce a message

---
+++ activemq/java-ee/customer-producer-java-ee-sample/src/main/java/de/sample/messaging/javaee/JaxRsApplication.java
@@ -0,0 +1,17 @@
+package de.sample.messaging.javaee;
+
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@OpenAPIDefinition(
+  info = @Info(
+    version = "1",
+    title = "Message Producer API"
+  )
+)
+@ApplicationPath("api/v1")
+public class JaxRsApplication extends Application {
+}
---
+++ activemq/java-ee/customer-producer-java-ee-sample/src/main/java/de/sample/messaging/javaee/MessageDto.java
@@ -0,0 +1,10 @@
+package de.sample.messaging.javaee;
+
+import lombok.Data;
+
+@Data
+public class MessageDto {
+
+    private String message;
+
+}
